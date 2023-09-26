fc37ca52a798ef502f22b2285b6e0351d034f020
SliferGod <97369432+SliferGod@users.noreply.github.com>
2023-09-26 18:45:17 -0400

    Basic Player Movement with the setup for a State Machine.
    Player go brrrrrr

--- Player.gd
+++ Player.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+#Based State Machine
+var state : String = "MOVEMENT"
+#Movement
 @export var speed : float = 500
-var speedBuildUp : float = 0
+var speedBuildUp : float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +13,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if state == "MOVEMENT":
+		_Movement(delta)
+	else:
+		print("YOUR STATE IS INVALID BUT THATS OKAY, I BELIEVE IN YOU!!!!!")
+
+func _Movement(delta):
+	var direction = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
+	position.x += direction * speed * delta * speedBuildUp
+	if direction and speedBuildUp <= 1.5:
+		speedBuildUp += delta
+	elif not direction:
+		speedBuildUp = 1
